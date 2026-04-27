f8f4d4bcab5afc004adb33fac26c90bba5241e04
fabien995 <fabien.sonpar@gmail.com>
2026-04-27 19:51:41 +0200

    github pages source

--- build/copy.ps1
+++ build/copy.ps1
@@ -1,1 +1,2 @@
 Copy-Item -Path "../src/*" -Destination C:/xampp/htdocs/water-diseases -Recurse -Force
+Copy-Item -Path "../pages-src/" -Destination C:/xampp/htdocs/water-diseases -Recurse -Force
---
+++ build/github-pages.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path "../src/html/index.html" -Destination "../pages-src/" -Recurse -Force
+Copy-Item -Path "../src/*" -Destination "../pages-src/" -Recurse -Force
